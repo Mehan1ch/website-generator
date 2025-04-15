e5a78b9b886f7e02bc375f647c8189841aeacd82
Péter Hanich <hanipet@gmail.com>
2025-04-15 14:32:04 +0200

    Add simple login form

--- Application/Frontend/vite.config.ts
+++ Application/Frontend/vite.config.ts
@@ -4,13 +4,12 @@ import {TanStackRouterVite} from '@tanstack/router-plugin/vite'
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
         TanStackRouterVite({target: 'react', autoCodeSplitting: true}),
-        react(),
         tailwindcss(),
+        react(),
         // ...,
     ],
     resolve: {
