c3570772bf1bb8e8350300dac22dff3db17be3a3
Péter Hanich <hanipet@gmail.com>
2025-05-19 20:41:21 +0200

    feat: add app wrapper

--- Application/Frontend/src/routes/index.tsx
+++ Application/Frontend/src/routes/index.tsx
@@ -1,8 +1,6 @@
-import {SidebarInset, SidebarProvider} from "@/components/ui/sidebar";
 import "../index.css"
 import {createFileRoute} from "@tanstack/react-router";
-import {AppSidebar} from "@/components/app-sidebar.tsx";
-import {Separator} from "@/components/ui/separator.tsx";
+import AppWrapper from "@/components/app-wrapper.tsx";
 
 export const Route = createFileRoute("/")({
     component: Index
@@ -10,20 +8,11 @@ export const Route = createFileRoute("/")({
 
 function Index() {
     return (
-        <SidebarProvider>
-            <AppSidebar/>
-            <SidebarInset>
-                <header
-                    className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-                    <div className="flex items-center gap-2 px-4">
-
-                        <Separator orientation="vertical" className="mr-2 h-4"/>
-                    </div>
-                </header>
-                <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-                </div>
-            </SidebarInset>
-        </SidebarProvider>
+        <AppWrapper header={"Dashboard"} link={"/"}>
+            <div className="flex flex-1 items-center justify-center">
+                <h1 className="text-2xl font-bold">Welcome to the Dashboard</h1>
+            </div>
+        </AppWrapper>
     )
 }
 
