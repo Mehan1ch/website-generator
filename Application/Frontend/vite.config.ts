e1c165c2ac1d676d1c62828a08721088a96e35b1
Péter Hanich <hanipet@gmail.com>
2025-10-07 20:02:21 +0200

    fix: update tanstack router vite config to new version

--- Application/Frontend/vite.config.ts
+++ Application/Frontend/vite.config.ts
@@ -1,13 +1,16 @@
 import path from "path"
 import {defineConfig} from 'vite'
-import {TanStackRouterVite} from '@tanstack/router-plugin/vite'
+import tanstackRouter from '@tanstack/router-plugin/vite'
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
-        TanStackRouterVite({target: 'react', autoCodeSplitting: true}),
+        tanstackRouter({
+            target: 'react',
+            autoCodeSplitting: true,
+        }),
         tailwindcss(),
         react(),
         // ...,
