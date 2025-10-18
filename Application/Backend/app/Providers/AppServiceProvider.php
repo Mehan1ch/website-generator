79de283763d5be58bded19ccacbd9cd7a9a3f712
Péter Hanich <hanipet@gmail.com>
2025-10-18 14:07:58 +0200

    fix: OpenAPI spec generation, implement UserResource

--- Application/Backend/app/Providers/AppServiceProvider.php
+++ Application/Backend/app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\ServiceProvider;
+use Knuckles\Camel\Extraction\ExtractedEndpointData;
+use Knuckles\Scribe\Scribe;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +24,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (class_exists(Scribe::class)) {
+            Scribe::beforeResponseCall(function () {
+                // Customise the request however you want (e.g. custom authentication)
+                $user = User::factory()->make();
+                Auth::login($user);
+            });
+        }
     }
 }
--- Application/Backend/routes/api.php
+++ Application/Backend/routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+/**
+ * User
+ *
+ * Get the authenticated user.
+ */
 Route::get('/user', function (Request $request) {
-    return $request->user();
+    return new UserResource($request->user());
 })->middleware('auth:sanctum');
