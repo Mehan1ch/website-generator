d521a57f7378890465178cbb4542a2853ef9f663
Péter Hanich <hanipet@gmail.com>
2025-10-20 21:58:25 +0200

    feat: install tanstack query eslint plugin

--- Application/Frontend/eslint.config.js
+++ Application/Frontend/eslint.config.js
@@ -4,9 +4,10 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import pluginRouter from '@tanstack/eslint-plugin-router';
+import pluginQuery from '@tanstack/eslint-plugin-router';
 
 export default tseslint.config({ignores: ['dist']}, {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, ...pluginRouter.configs['flat/recommended'],],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, ...pluginRouter.configs['flat/recommended'], ...pluginQuery.configs['flat/recommended']],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
         ecmaVersion: 2020, globals: globals.browser,
