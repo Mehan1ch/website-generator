a148057002ed4ec4aad9a76d7743fc9974ed6a7f
Péter Hanich <hanipet@gmail.com>
2025-10-22 00:10:12 +0200

    fix: register page redirect

--- Application/Frontend/src/routes/_auth/register.tsx
+++ Application/Frontend/src/routes/_auth/register.tsx
@@ -1,4 +1,4 @@
-import {createFileRoute, redirect} from '@tanstack/react-router';
+import {createFileRoute, Link, redirect} from '@tanstack/react-router';
 import {SignupForm} from "@/components/forms/signup-form.tsx";
 import {GalleryVerticalEnd} from "lucide-react";
 import {redirectOnlySearchSchema} from "@/types/search.ts";
@@ -19,13 +19,15 @@ function RouteComponent() {
     return (
         <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div className="flex w-full max-w-sm flex-col gap-6">
-                <a href="#" className="flex items-center gap-2 self-center font-medium">
+                <Link to={"/"} className="flex items-center gap-2 self-center font-medium" search={{
+                    redirect: redirect
+                }}>
                     <div
                         className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
                         <GalleryVerticalEnd className="size-4"/>
                     </div>
                     {appTitle}
-                </a>
+                </Link>
                 <SignupForm redirect={redirect}/>
             </div>
         </div>
