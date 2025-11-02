836e2d0ed8576ad60685dc3b7650fd0b44ce24cd
Péter Hanich <hanipet@gmail.com>
2025-11-02 21:58:44 +0100

    feat: add default view transition

--- Application/Frontend/src/main.tsx
+++ Application/Frontend/src/main.tsx
@@ -1,14 +1,14 @@
-import ReactDOM from 'react-dom/client'
-import {createRouter, RouterProvider} from '@tanstack/react-router'
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
-import {routeTree} from './routeTree.gen'
+import ReactDOM from 'react-dom/client';
+import {createRouter, RouterProvider} from '@tanstack/react-router';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {routeTree} from './routeTree.gen';
 import {StrictMode} from "react";
 import {AuthProvider} from "@/providers/auth-provider.tsx";
 import {ThemeProvider} from "@/providers/theme-provider.tsx";
 import {useAuth} from "@/hooks/use-auth.tsx";
 import {Toaster} from "@/components/ui/sonner.tsx";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 // Set up a Router instance
 const router = createRouter({
@@ -18,27 +18,28 @@ const router = createRouter({
         auth: undefined!,
         getTitle: () => "",
     },
+    defaultViewTransition: true,
     defaultPreload: 'intent',
     // Since we're using React Query, we don't want loader calls to ever be stale
     // This will ensure that the loader is always called when the route is preloaded or visited
     defaultPreloadStaleTime: 0,
     scrollRestoration: true,
-})
+});
 
 // Register things for type safety
 declare module '@tanstack/react-router' {
     interface Register {
-        router: typeof router
+        router: typeof router;
     }
 }
 
 // eslint-disable-next-line react-refresh/only-export-components
 function InnerApp() {
-    const auth = useAuth()
+    const auth = useAuth();
     return <RouterProvider router={router} context={{
         queryClient,
         auth
-    }}/>
+    }}/>;
 }
 
 // eslint-disable-next-line react-refresh/only-export-components
@@ -52,16 +53,16 @@ function App() {
                 </AuthProvider>
             </ThemeProvider>
         </QueryClientProvider>
-    )
+    );
 }
 
-const rootElement = document.getElementById('root')!
+const rootElement = document.getElementById('root')!;
 
 if (!rootElement.innerHTML) {
-    const root = ReactDOM.createRoot(rootElement)
+    const root = ReactDOM.createRoot(rootElement);
     root.render(
         <StrictMode>
             <App/>
         </StrictMode>
-    )
+    );
 }
