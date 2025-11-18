712c037e128c156f34e2f06d002640bce0d00d32
Péter Hanich <hanipet@gmail.com>
2025-11-18 20:46:56 +0100

    feat: remove unnecessary backend deps, update packages

--- Application/Backend/app/Models/Page.php
+++ Application/Backend/app/Models/Page.php
@@ -10,7 +10,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Carbon;
-use LZCompressor\LZString;
 use Spatie\MediaLibrary\MediaCollections\File;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
