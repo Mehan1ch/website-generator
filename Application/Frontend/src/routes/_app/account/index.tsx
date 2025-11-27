b39291f576fcb3a99178aeafc1ebffaf7fef79b7
Péter Hanich <hanipet@gmail.com>
2025-11-28 00:57:44 +0100

    feat: account tab query params

--- Application/Frontend/src/routes/_app/account/index.tsx
+++ Application/Frontend/src/routes/_app/account/index.tsx
@@ -1,4 +1,4 @@
-import {createFileRoute} from '@tanstack/react-router';
+import {createFileRoute, useNavigate} from '@tanstack/react-router';
 import Appearance from "@/routes/_app/account/-components/appearance.tsx";
 import {ProfileForm} from "@/routes/_app/account/-components/profile-form.tsx";
 import {AvatarUpload} from "@/routes/_app/account/-components/avatar-upload.tsx";
@@ -7,8 +7,16 @@ import {DeleteAccountForm} from "@/routes/_app/account/-components/delete-accoun
 import PasswordForm from "@/routes/_app/account/-components/password-form.tsx";
 import {Tabs, TabsContent, TabsContents, TabsList, TabsTrigger} from "@/components/ui/tabs";
 import {RouteTitle} from "@/components/blocks/route-title.tsx";
+import {z} from "zod";
+
+type AccountTab = "appearance" | "profile" | "avatar" | "verification" | "password" | "delete";
+
+const accountSearchSchema = z.object({
+    tab: z.literal(["appearance", "profile", "avatar", "verification", "password", "delete"]).default("appearance"),
+});
 
 export const Route = createFileRoute('/_app/account/')({
+    validateSearch: accountSearchSchema,
     beforeLoad: () => {
         return {
             getTitle: () => 'Account',
@@ -18,10 +26,22 @@ export const Route = createFileRoute('/_app/account/')({
 });
 
 function Account() {
+    const {tab} = Route.useSearch();
+    const navigate = useNavigate({from: Route.fullPath});
+
+    const handleTabChange = async (value: AccountTab) => {
+        await navigate({
+            search: (old) => ({
+                ...old,
+                tab: value,
+            }),
+        });
+    };
+
     return <div>
         <RouteTitle title={"Settings"} description={"Manage your profile and account settings"}/>
         <div className="flex lg:w-1/2 w-full flex-col gap-6">
-            <Tabs defaultValue="appearance">
+            <Tabs value={tab} onValueChange={handleTabChange}>
                 <TabsList>
                     <TabsTrigger value="appearance">Appearance</TabsTrigger>
                     <TabsTrigger value="profile">Profile</TabsTrigger>
