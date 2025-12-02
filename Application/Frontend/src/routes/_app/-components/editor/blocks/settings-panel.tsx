022ad55a318529e1cf6f899b5d414482c5b41c39
Péter Hanich <hanipet@gmail.com>
2025-12-02 17:50:22 +0100

    feat: redesign settings panel

--- Application/Frontend/src/routes/_app/-components/editor/blocks/settings-panel.tsx
+++ Application/Frontend/src/routes/_app/-components/editor/blocks/settings-panel.tsx
@@ -1,12 +1,11 @@
-import {Card, CardContent} from "@/components/ui/card.tsx";
-import {Button} from "@/components/ui/button.tsx";
 import {useEditor} from "@craftjs/core";
 import {createElement} from "react";
 import {EditorDisabledEmpty} from "@/routes/_app/-components/editor/blocks/editor-disabled-empty.tsx";
 import {EditorSelectedEmpty} from "@/routes/_app/-components/editor/blocks/editor-selected-empty.tsx";
+import {Badge} from "@/components/ui/badge.tsx";
 
 export const SettingsPanel = () => {
-    const {actions, selected, enabled} = useEditor((state, query) => {
+    const {selected, enabled} = useEditor((state, query) => {
         const [currentNodeId] = state.events.selected;
         let selected;
 
@@ -25,33 +24,19 @@ export const SettingsPanel = () => {
         };
     });
 
+    const getDisplayName = (name: string) => {
+        return name.startsWith('Editor') ? name.slice(6) : name;
+    };
+
     if (!enabled) return <EditorDisabledEmpty/>;
 
     return selected ? (
-        <Card className="bg-muted mt-2 px-4 py-4">
-            <CardContent className="flex flex-col space-y-4 p-0">
-                <div>
-                    <div className="flex items-center justify-between pb-2">
-                        <span className="text-base font-medium">Selected</span>
-                        <span
-                            className="px-2 py-0.5 rounded-full bg-primary text-primary-foreground text-xs font-semibold">{selected.name}</span>
-                    </div>
-                </div>
-                {
-                    selected.settings && createElement(selected.settings)
-                }
-                {
-                    selected.isDeletable ? (
-                        <Button variant="default" className="w-full"
-                                onClick={() => {
-                                    actions.delete(selected.id);
-                                }}
-                        >
-                            Delete
-                        </Button>
-                    ) : null
-                }
-            </CardContent>
-        </Card>
+        <div className="flex flex-col space-y-4 p-4">
+            <div className="flex items-center justify-between pb-2">
+                <span className="text-sm font-medium">Selected</span>
+                <Badge variant="default">{getDisplayName(selected.name)}</Badge>
+            </div>
+            {selected.settings && createElement(selected.settings)}
+        </div>
     ) : <EditorSelectedEmpty/>;
 };
