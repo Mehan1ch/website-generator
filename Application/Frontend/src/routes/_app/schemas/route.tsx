c8deb2153cd519f3cdc79f2649ba8e919662269f
Péter Hanich <hanipet@gmail.com>
2025-12-03 18:27:52 +0100

    fix: redirect if email not verified

--- Application/Frontend/src/routes/_app/schemas/route.tsx
+++ Application/Frontend/src/routes/_app/schemas/route.tsx
@@ -1,7 +1,17 @@
-import {createFileRoute, Outlet} from '@tanstack/react-router';
+import {createFileRoute, Outlet, redirect} from '@tanstack/react-router';
+import {toast} from "sonner";
 
 export const Route = createFileRoute('/_app/schemas')({
-    beforeLoad: () => {
+    beforeLoad: ({context: {auth: {user}}}) => {
+        if (!user?.email_verified_at) {
+            toast.error("Please verify your email address to access schemas.");
+            throw redirect({
+                to: '/login',
+                search: {
+                    redirect: location.href,
+                },
+            });
+        }
         return {
             getTitle: () => 'Schemas',
         };
--- Application/Frontend/src/routes/_app/sites/route.tsx
+++ Application/Frontend/src/routes/_app/sites/route.tsx
@@ -1,7 +1,17 @@
-import {createFileRoute, Outlet} from '@tanstack/react-router';
+import {createFileRoute, Outlet, redirect} from '@tanstack/react-router';
+import {toast} from "sonner";
 
 export const Route = createFileRoute('/_app/sites')({
-    beforeLoad: () => {
+    beforeLoad: ({context: {auth: {user}}}) => {
+        if (!user?.email_verified_at) {
+            toast.error("Please verify your email address to access schemas.");
+            throw redirect({
+                to: '/login',
+                search: {
+                    redirect: location.href,
+                },
+            });
+        }
         return {
             getTitle: () => 'Sites',
         };
